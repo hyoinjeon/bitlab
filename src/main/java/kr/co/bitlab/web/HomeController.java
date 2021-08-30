c4ded4090e948f5034cb4603068fca0cd8c9b3b0
전효인 <hiq@kakao.com>
2021-08-30 10:48:29 +0900

    feature(architect) : set architect

--- src/main/java/kr/co/bitlab/web/HomeController.java
+++ src/main/java/kr/co/bitlab/web/HomeController.java
@@ -8,8 +8,37 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        System.out.println("home controller start");
         return "index";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+
+    @GetMapping("/service")
+    public String service() {
+        return "service";
+    }
+
+    @GetMapping("/contact")
+    public String contact() {
+        return "contact";
+    }
+
+    @GetMapping("/solutions/test1")
+    public String solution1() {
+        return "solution";
+    }
+
+    @GetMapping("/solutions/test2")
+    public String solution2() {
+        return "solution";
+    }
+
+    @GetMapping("/solutions/test3")
+    public String solution3() {
+        return "solution";
     }
 
 }
